ef2bc6dad29a21bdbaa8df7b0f48c551c80c4ae5
Azharafridi <99399706+Azharafridi@users.noreply.github.com>
2022-12-09 00:01:46 -0800

    Add files via upload

---
+++ min_max_in_list.py
@@ -0,0 +1,14 @@
+# python has min and max methods that give minimum and maximum number in a list
+
+def minimum():
+    numbers = [2, 4, 8, 4, 23, 5, 6]
+    # display minimum in this list
+    print('the minimum number in the list is : ', min(numbers))
+
+minimum()
+
+# method for maximum
+def maximum():
+    numbers = [2, 4, 8, 4, 23, 5, 6]
+    print('the maximum number in the list is : ', max(numbers))
+maximum()
